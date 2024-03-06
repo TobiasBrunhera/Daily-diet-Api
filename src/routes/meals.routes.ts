7ef8b454831028949bf4b8597a6cdda000492400
Tobias <tobiasboliveira@gmail.com>
2024-03-06 14:02:52 -0300

    project in progress

--- src/routes/meals.routes.ts
+++ src/routes/meals.routes.ts
@@ -2,51 +2,36 @@ import { FastifyInstance } from 'fastify'
 import { randomUUID } from 'crypto'
 import { z } from 'zod'
 import { knex } from '../database'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function mealsRoutes(app: FastifyInstance) {
   app.post(
     '/',
+    { preHandler: [checkSessionIdExists] },
     async (request, reply) => {
       const createMealBodySchema = z.object({
         name: z.string(),
         description: z.string(),
         isOnDiet: z.boolean(),
         date: z.coerce.date(),
-      });
+      })
 
-      try {
-        const { name, description, isOnDiet, date } = createMealBodySchema.parse(request.body);
+      const { name, description, isOnDiet, date } = createMealBodySchema.parse(
+        request.body,
+      )
 
-        // Verifica se o ID do usuário está presente na solicitação
-        if (!request.user?.id) {
-          return reply.status(401).send({ message: 'Unauthorized' });
-        }
+      await knex('meals').insert({
+        id: randomUUID(),
+        name,
+        description,
+        is_on_diet: isOnDiet,
+        date: date.getTime(),
+        user_id: request.user?.id,
+      })
 
-        console.log(createMealBodySchema)
-
-        // Verifica se já existe uma refeição para o usuário
-        const verifyMeal = await knex('meals').where({ user_id: request.user.id }).first();
-        if (verifyMeal) {
-          return reply.status(400).send({ message: 'User already has a meal' });
-        }
-
-        // Insere a refeição no banco de dados
-        const meal = {
-          id: randomUUID(),
-          name,
-          description,
-          is_on_diet: isOnDiet,
-          date: date.getTime(),
-          user_id: request.user.id,
-        };
-
-        const insertedMeal = await knex('meals').insert(meal).returning('*');
-        return reply.status(201).send(insertedMeal);
-      } catch (error) {
-        return reply.status(400).send();
-      }
-    }
-  );
+      return reply.status(201).send()
+    },
+  )
 
   app.get(
     '/',
