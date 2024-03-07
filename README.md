e0fa499b72bc71c8feacb6909c27baf81b9c950c
Tobias <tobiasboliveira@gmail.com>
2024-03-07 16:14:31 -0300

    final project

---
+++ README.md
@@ -0,0 +1,19 @@
+post createUser - {
+	"name": "exemplo",
+	"email": "exemplo@gmail.com"
+}
+
+post createDiet - {
+	"name": "exemplo",
+	"description": "exemplo, exemplo, exemplo",
+	"isOnDiet": false or true,
+	"date": "2024-03-06",
+	"user_id": "id que retonar quando cria um user"
+}
+
+put - {
+	"name": "exemplo",
+	"description": "exemplo, exemplo, exemplo, exemplo",
+	"isOnDiet": false or true,
+	"date": "2024-03-06"
+}
--- src/routes/meals.routes.ts
+++ src/routes/meals.routes.ts
@@ -95,6 +95,26 @@ export async function mealsRoutes(app: FastifyInstance) {
     }
   )
 
+  app.delete(
+    '/mealId',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const paramsSchema = z.object({ mealId: z.string().uuid() })
+
+      const { mealId } = paramsSchema.parse(request.params)
+
+      const meal = await knex('meals').where({ id: mealId }).first()
+
+      if(!meal) {
+        return reply.status(404).send({ erro: 'Meal not found' })
+      }
+
+      await knex('meals').where({ id: mealId }).delete()
+
+      return reply.status(204).send()
+    }
+  )
+
   app.get(
     '/metrics',
     { preHandler: [checkSessionIdExists] },
